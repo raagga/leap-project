340913c3e8f6647d3e6852fe0054f1dbef56f23f
Ranveer Aggarwal <ranveeraggarwal.msft@gmail.com>
2016-07-21 15:18:23 +0530

    Added models

---
+++ Lingoine/Lingoine/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lingoine.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public string Place { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string SkypeId { get; set; }
+        public bool IsPremium { get; set; }
+    }
+}
---
+++ Lingoine/Lingoine/Models/UserLanguage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lingoine.Models
+{
+    public class UserLanguage
+    {
+        public int UserId { get; set; }
+        public int LanguageId { get; set; }
+        public int ProficiencyLevel { get; set; }
+        public float Rating { get; set; }
+    }
+}
