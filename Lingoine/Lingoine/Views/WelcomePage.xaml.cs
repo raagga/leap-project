38a5a87259a21878d7b9685b4b07aba6fa1cb43f
pallavisethi <pallavisethi.cse@gmail.com>
2016-07-21 15:19:08 +0530

    Welcome page

--- Lingoine/Lingoine/Views/WelcomePage.xaml.cs
+++ Lingoine/Lingoine/Views/WelcomePage.xaml.cs
@@ -13,20 +13,33 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Lingoine
+namespace Lingoine.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class WelcomePage : Page
     {
-        public MainPage()
+        public WelcomePage()
         {
             this.InitializeComponent();
         }
 
-        
+        private void SignInClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void SignUpClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Views.SignUp));
+        }
+        private void ExploreClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
